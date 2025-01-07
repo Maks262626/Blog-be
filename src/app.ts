5a66e3005345d6aec112c3d7a150f81735915a23
maxum2003la <maxum2003la@gmail.com>
2025-01-07 23:08:31 +0200

    fix: cors

--- src/app.ts
+++ src/app.ts
@@ -17,7 +17,6 @@ const app = express();
 
 app.use(express.json());
 app.use(cookieParser())
-app.use(cors());
 app.use(cors({
   origin: 'http://localhost:5173', 
   credentials: true,              
