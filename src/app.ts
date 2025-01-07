7884309125eccfeb61d716f364749bd33e893503
maxum2003la <maxum2003la@gmail.com>
2025-01-07 23:23:40 +0200

    fix: add logs

--- src/app.ts
+++ src/app.ts
@@ -19,7 +19,8 @@ app.use(express.json());
 app.use(cookieParser())
 app.use(cors({
   origin: 'http://localhost:5173', 
-  credentials: true,              
+  credentials: true,    
+  methods: ['GET', 'POST', 'PUT', 'DELETE']          
 }));
 app.use('/upload', express.static('upload'));
 
--- src/middlewares/verifyToken.ts
+++ src/middlewares/verifyToken.ts
@@ -5,6 +5,7 @@ import { NextFunction, Request, Response } from 'express';
 import admin from 'firebase-admin';
 
 export const verifyToken = async (req: Request, res: Response, next: NextFunction) => {
+  console.log('Cookies:', req.cookies);
   const idToken = req.cookies?.access_token;
   if (!idToken) {
     res.status(403).json({ error: 'No token provided' });
